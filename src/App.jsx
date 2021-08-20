d022ed148f9387549d35681748a3983b8b0b5f01
Anirudh Cowlagi <acowlagi@sas.upenn.edu>
2021-08-19 22:19:21 -0400

    almost done topbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import Intro from "./components/intro/Intro";
 import Projects from "./components/projects/Projects";
 import Topbar from "./components/topbar/Topbar";
 import "./app.scss"
+import { useState } from "react";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
-     <Topbar/>
+     <Topbar menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <About/>
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,33 +3,40 @@ import "./topbar.scss"
 import PhoneIcon from '@material-ui/icons/Phone';
 import MailIcon from '@material-ui/icons/Mail';
 
-export default function Topbar() {
+export default function Topbar({menuOpen, setMenuOpen}) {
     return (
-        <div className = "topbar">
+        <div className = {"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro"> 
-                    <img src="assets/logo.png" alt="logo" className = "logo"/> 
+                    <img src= {!menuOpen ? "assets/logo.png" : "assets/logoalt2.png"} alt="logo" className = "logo"/> 
                     </a>
 
                     <div className="itemContainer">
-                        <PhoneIcon className = "icon"/>
-                    <span>(734) 747-4331</span>
+                        <a href="tel:+7347474331"> 
+                            <PhoneIcon className = "icon"/>
+                        </a>
+                        
+                        <span> 
+                            <a href="tel:+7347474331"> (734) 747-4331 </a>
+                         </span>
+                    </div>
+                    
+                    <div className="itemContainer">
+                        <a href="mailto:acowlagi@sas.upenn.edu"> 
+                            <MailIcon className = "icon"/>
+                        </a>
+                        
+                        <span> 
+                            <a href="mailto:acowlagi@sas.upenn.edu">  acowlagi@sas.upenn.edu
+                            </a>
+                         </span>
                     </div>
 
-                    <div className="itemContainer">
-                        <MailIcon className = "icon"/>
-                        <span> 
-                            <a href="mailto:acowlagi@sas.upenn.edu" >
-                            acowlagi@sas.upenn.edu
-                            </a>
-                        </span>
-                    
-                    </div>
                 </div>
                 
                 <div className="right">
-                    <div className="hamburger">
+                    <div className="hamburger" onClick ={()=>setMenuOpen(!menuOpen)}>
                         <span className="line1"></span>
                         <span className="line2"></span>
                         <span className="line3"></span>
