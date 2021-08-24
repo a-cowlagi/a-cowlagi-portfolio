057e9461be04e4102ceaddbb4fb172602b51086d
Anirudh Cowlagi <acowlagi@sas.upenn.edu>
2021-08-24 16:24:22 -0400

    portoflio changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,26 +1,28 @@
-import ScrollAnimation from "react-animate-on-scroll"
+import Fade from 'react-reveal/Fade';
 import "./about.scss"
 
 
 export default function About() {
     return (
-        
-        <div className = "about" id = "about">
+        <Fade bottom>
+            <div className = "about" id = "about">
                
-            <div className="leftpad"></div>
-            <div className="left">
-                    <div className="imgContainer">
-                            <img src="assets/profile3.png" alt="" />
-                        
-                        </div>
-            </div>
-
-            <div className="right">
-                <div className="wrapper">
-                    <h1>About Me</h1>
-                </div>
-            </div>
-
-        </div>
+               <div className="leftpad"></div>
+               <div className="left">
+                       <div className="imgContainer">
+                               <img src="assets/profile3.png" alt="" />
+                           
+                           </div>
+               </div>
+   
+               <div className="right">
+                   <div className="wrapper">
+                       <h1>About Me</h1>
+                   </div>
+               </div>
+   
+           </div>
+        </Fade>
+        
     )
 }
