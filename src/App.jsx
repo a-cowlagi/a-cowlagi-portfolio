deb1d09c6b19c0345dbef8b41c16c776a3591e28
Anirudh Cowlagi <acowlagi@sas.upenn.edu>
2022-01-04 23:29:53 -0500

    added education to app

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Intro from "./components/intro/Intro";
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
+import Education from "./components/education/Education";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 
@@ -16,13 +17,14 @@ function App() {
   const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
-
-      <BrowserRouter>
+      
+    <BrowserRouter>
         <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Routes>
           <Route exact path="/#intro" element={<Intro />} />
           <Route exact path="/#about" element={<About />} />
+          <Route exact path="/#education" element={<Education />} />
           <Route exact path="/#experience" element={<Experience />} />
           <Route exact path="/#projects" element={<Projects />} />
           <Route exact path="/#contact" element={<Contact />} />
