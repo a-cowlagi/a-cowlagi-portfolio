b8a1174e4da512560f4b79f03be428734f77380c
Anirudh Cowlagi <acowlagi@sas.upenn.edu>
2022-12-29 18:49:53 -0500

    fixed assets

--- src/Data.js
+++ src/Data.js
@@ -4,8 +4,8 @@ export const allProjects = [
     title: "Does Data Geometry Control Neural Geometry?",
     text: "Studying deep network over-parameterization using the Fisher Information Matrix (FIM). <br><br> Discovered surprising trends in the structure of the FIM eigenspectrum and the topology of model predictions. <br><br> <div class = 'inner' style = 'z-index: 20'> <a href= 'https://aaai.org/Conferences/AAAI-22/student-abstract-and-poster-program/' target = '_blank'> </div> Published </a> in AAAI-2022 as a Student Abstract and further selected as 1 of 20 finalist papers.",
     img:
-      process.env.PUBLIC_URL + "assets/eigenspectra.png",
-    link: process.env.PUBLIC_URL + "/Data-Geometry-Neural-Geometry.pdf",
+       "assets/eigenspectra.png",
+    link:  "/Data-Geometry-Neural-Geometry.pdf",
     
   },
   {
@@ -22,14 +22,14 @@ export const allProjects = [
     title: "Citadel Correlation One Data Open Championship",
     text: "<a href = 'https://drive.google.com/file/d/15FPujKoWgMyVR9WH3wJplt73qx2rrqGm/view?usp=share_link' target = '_blank'>Summer Invitational (1st Place)</a>: Conducted an in-depth analysis of the risk assessment methodology employed by peer-peer lenders, using a modified nearest neighbors model. <br><br> <a href = 'https://drive.google.com/file/d/1Zv0SI1A1hsJL8wY-9PHiPHu309Ct31bE/view?usp=share_link' target = '_blank'>Championship (3rd Place)</a>: Developed a robust methodology to improve the budgetary policy decisions of post-secondary institutions using constrained optimization on a low-dimensional manifold.",
     img:
-      process.env.PUBLIC_URL + "assets/mca_embedding.png",
+       "assets/mca_embedding.png",
     link: "https://github.com/a-cowlagi/citadel-summer-invitational22",    
   },
   {
     id: 4,
     title: "AstroTrack",
     text: "An efficient Python-based pipeline to aid with the detection, recovery, and characterization of minor planets in the solar system (asteroids, trans-Neptunian objects, Kuiper Belt Objects) using the DES and DEEP surveys.  <br><br>Single night analysis results: Recovered ~480 known asteroids across exposures, detection of ~1,000 new objects.  <a href = 'https://abstracts.societyforscience.org/Home/FullAbstract?ISEFYears=0%2C&Category=Any%20Category&AllAbstracts=True&FairCountry=Any%20Country&FairState=Any%20State&ProjectId=19496' target = '_blank'> ISEF Finalist </a>",
-    img: process.env.PUBLIC_URL + "assets/astrotrack.png",
+    img:  "assets/astrotrack.png",
     link: "https://gitfront.io/r/user-7866516/bc4cff16377033ad8855f71e915117cfcf7855d3/AstroTrack/",
   },
   {
@@ -37,7 +37,7 @@ export const allProjects = [
     title: "StructuralGT",
     text: "Aided with the development of a <a href = 'https://pypi.org/project/StructuralGT/' target = '_blank'>Python package </a> to automate the analysis of structural networks (macro/microscale) by converting greyscale representations (e.g. SEM/TEM images) to graphs using the NetworkX library. <br><br> Recognized in Acknowledgements of <a href = 'https://pubs.acs.org/doi/10.1021/acsnano.1c04711' target = '_blank'>Structural Analysis of Nanoscale Network Materials Using Graph Theory (ACS Nano 2021, 15, 8) </a>. ",
     img:
-      process.env.PUBLIC_URL + "assets/structuralgt.png",
+       "assets/structuralgt.png",
     link: "https://www.biorxiv.org/content/10.1101/2021.04.07.438877v1.full"
   },
   {
@@ -45,7 +45,7 @@ export const allProjects = [
     title: "AI@Penn, WUDAC",
     text: "Organized <a href = 'https://aipenn.org/aihacks' target = '_blank'> datathon </a> at the University of Pennsylvania sponsored by fintech/e-commerce companies attracting 100+ teams <br><br> Designed and taught workshop on sentiment analysis to 30+ Penn undergraduate students <br><br>Worked with a Philadelphia-based startup (Highlight) to develop a robust system for product review evaluation with 90+% OOS accuracy",
     img:
-      process.env.PUBLIC_URL + "assets/aipennwudac.png",
+       "assets/aipennwudac.png",
     link: "https://gitfront.io/r/user-7866516/e40d90e09f12584ba430ee018c714ebc691b7486/AI-Penn-and-WUDAC/"
   },
   {
@@ -53,7 +53,7 @@ export const allProjects = [
     title: "IOT-Based Solar Monitoring",
     text: "Constructed a novel IOT-based monitoring and feedback system for use with off-grid photovoltaic systems. <br><br> A long-range, Arduino-based device able to dynamically adjust power production to accomodate for real-time conditions and adapt to user energy consumption patterns. <br><br> Category winner (Energy) at Michigan Science and Engineering Fair",
     img:
-      process.env.PUBLIC_URL + "assets/iot.png",
+       "assets/iot.png",
     link: "https://drive.google.com/drive/folders/13q-R1LtKgJ6YbgFQYetQewGDOrZpF2_a?usp=sharing",
   
   },
@@ -62,7 +62,7 @@ export const allProjects = [
     title: "Billiard Balls and Decimal Expansions",
     text: "Projects completed at the University of Michigan, in an inquiry-based course exploring some open problems in mathematics. <br><br>• Billiard balls is an exploration of the mathematics of the trajectory of a billiard ball undergoing collisions with walls. <br><br>• Decimal expansions investigates the periods of decimal expansions of rational numbers.",
     img:
-      process.env.PUBLIC_URL + "assets/mathresearch.png",
+       "assets/mathresearch.png",
     link: "https://drive.google.com/drive/folders/1xT9Ds7hNejW5dVYZksuPWH8sdXXNB-kJ?usp=sharing"
   },
   {
@@ -70,7 +70,7 @@ export const allProjects = [
     title: "Problem Solving in Physics",
     text: "Created and taught an introductory course in physics and problem solving to 20+ local middle and high school students <br><br> Developed 18 interactive lecture sessions and 10 hands-on problem sets with extensive feedback",
     img:
-      process.env.PUBLIC_URL + "assets/ipsp.png",
+       "assets/ipsp.png",
     link: "https://drive.google.com/file/d/1zTOqgrT5WAkS9fnQmBErSlS7Kah1744N/view?usp=sharing"
   }
 
